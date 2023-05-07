2c10af5a91cc9c10829e046209a5b7aef105566a
Guilherme <guilherme.lima@receiv.it>
2023-05-06 22:18:00 -0300

    Criando endpoint de DELETE produto

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -53,4 +53,15 @@ class ProductController extends Controller
 
 
     }
+
+    public function destroy (Product $product){
+
+        $product->delete();
+
+        return response() -> json([
+            'status' => '200',
+            'mensagem' => 'Produto deletado'
+        ], 200);
+
+    }
 }
